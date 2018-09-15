002c21c1da9c71da7a12b9c6c27b63bad5649103
niuxw <niuxw@iastate.edu>
2018-09-14 19:36:25 -0500

    Add formatting and documentation to SiteReader

--- src/main/java/club/systemsdevelopment/webscraper/SiteReader.java
+++ src/main/java/club/systemsdevelopment/webscraper/SiteReader.java
@@ -18,34 +18,37 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
-
+/**
+ *  A utility class for reading the raw web pages.
+ */
 public class SiteReader {
-    private static final DateFormat timeFormat = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss z", Locale.US);
+    //private static final DateFormat timeFormat = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss z", Locale.US);
 
-    public static byte[] readFile(String host, String file, long time) throws IOException {
-
-        // use size limit in config
-        byte[] buffer = new byte[Configs.getMaxPageSize()];
+    /**
+     * Read a raw web pages.
+     *
+     * @param host the host name of the server to get the file
+     * @param file the file to get the server form.
+     * @return the bytes of the file.
+     * @throws IOException if a general I/O error happens.
+     */
+    public static byte[] readFile(String host, String file/*, long time*/) throws IOException {
+        byte[] buffer = new byte[Configs.getMaxPageSize()];// Initialize the buffer of largest size
         HttpURLConnection connection = (HttpURLConnection) new URL("http", host, file).openConnection();
+
         connection.setRequestMethod("GET");
         connection.setRequestProperty("User-Agent", Configs.getUserAgent());
         connection.setRequestProperty("Accept", "text/html");
         connection.setRequestProperty("Accept-Language", "en-US");
         connection.setRequestProperty("Upgrade-Insecure_Requests", "1");
-        connection.setRequestProperty("If-Modified_Since", timeFormat.format(new Date(time)));
+        //connection.setRequestProperty("If-Modified_Since", timeFormat.format(new Date(time)));
         InputStream inputStream = connection.getInputStream();
-        int read, total = 0, readSize = 0;
+
+        int read, total = 0;
         while ((read = inputStream.read(buffer, total, buffer.length - total)) != -1) {
             total += read;
             if (total >= buffer.length) throw new RuntimeException();
         }
-
-        //System.out.println(new String(byteBuffer.array(), 0, total, StandardCharsets.UTF_8));
-
         return buffer;
     }
 }
